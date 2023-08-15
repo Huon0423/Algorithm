9a2b530d78e8e30e6d5553c79d5f3c188effc3e9
Yunjeong Cho <dbswjd1413@naver.com>
2023-08-16 01:26:35 +0900

    a

--- test.py
+++ test.py
@@ -1,18 +1,11 @@
-# 줄을 선 사람의 수 n(1~1000)과 각 사람의 인출 시간 Pi(1~1000)가 주어진다.
-# 필요한 시간의 값을 구하는 방식은 1,1+2,1+2+3,1+2+3+4,1+2+3+4...+n
-# 앞순서가 중복으로 더해지므로 걸리는 시간이 가장 짧은 순으로 나열하면 된다.
-# 정렬된 인출 시간들을 누적 합 함수 cumsum을 사용해 구한다. > numpy 사용 불가
-# for 문으로 대체하여, 0부터 i까지의 합을 변수에 모두 더한다.
+# 정해진 개수를 배열로 저장해서 들어오는 값과 비교한다.
+# 두 수의 차이만큼을 배열에 저장하여 출력한다.
 
-# import numpy as np
+chess = [1,1,2,2,2,8]
+whitePiece = list(map(int, input().split()))
+answer= ""
 
-n = int(input())
-times = list(map(int, input().split()))
-times.sort()
-all_sum = 0
+for i in range(len(whitePiece)) :
+    answer = answer + str(chess[i] - whitePiece[i]) + " "
 
-# print(sum(np.cumsum(times)))
-for i in range(n) :
-    all_sum += sum(times[:i+1])
-
-print(all_sum)
+print(answer.strip())
