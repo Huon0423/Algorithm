d87fa56b0a5c77cd2754a94e99fc92e61fc676da
Huon0423 <97392183+Huon0423@users.noreply.github.com>
2023-08-21 21:38:41 +0900

    [Bronze II] Title: 블랙잭, Time: 96 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -6,8 +6,8 @@ n, m = map(int, input().split())
 cards = list(map(int, input().split()))
 answer = 0
 
-for i in range(n) :
-    for j in range(i+1, n) :
+for i in range(n-2) :
+    for j in range(i+1, n-1) :
         for k in range(j+1, n) :
             sum =  cards[i] + cards[j] + cards[k]
             if sum <= m :
