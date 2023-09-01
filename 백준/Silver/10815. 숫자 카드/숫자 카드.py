a5aba1d7504a2fbb1cbdcb76b2158dd02fdb29ba
Huon0423 <97392183+Huon0423@users.noreply.github.com>
2023-09-02 00:19:11 +0900

    [Silver V] Title: 숫자 카드, Time: 784 ms, Memory: 115404 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,17 +1,17 @@
-# dict를 사용하여 cards의 숫자와 checks의 숫자를 비교한다.
+# 시간초과 >> dict를 사용하여 cards의 숫자와 checks의 숫자를 비교한다.
 
 import sys
 
-N = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 cards = list(map(int, sys.stdin.readline().split()))
-M = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
 checks = list(map(int, sys.stdin.readline().split()))
 
-_dict = {}  # 속도는 dictionary!
-for i in range(len(cards)):
-    _dict[cards[i]] = 0  # 아무 숫자로 mapping
+_dict = {}
+for i in range(n):
+    _dict[cards[i]] = 0  # 0으로 초기화
 
-for j in range(M):
+for j in range(m):
     if checks[j] not in _dict:
         print(0, end=' ')
     else:
