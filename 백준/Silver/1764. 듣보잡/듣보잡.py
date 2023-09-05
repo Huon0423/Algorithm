b265763b51f4a4d5eb1de686a7ef0474f53e5cf7
Huon0423 <97392183+Huon0423@users.noreply.github.com>
2023-09-06 00:58:33 +0900

    [Silver IV] Title: 듣보잡, Time: 248 ms, Memory: 126680 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,11 +1,14 @@
+# 듣도 못한 사람과 보도 못한 사람을 각각 set에 넣어 저장한다.
+# 두 set 중 교집합을 찾아 sort하여 answer에 리스트로 저장한다.
+
 n,m = map(int, input().split())
 
-a = set()
+hear = set()
 for i in range(n) :
-    a.add(input())
-b = set()
+    hear.add(input())
+see = set()
 for i in range(m) :
-    b.add(input())
-answer = sorted(list(a&b))
+    see.add(input())
+answer = sorted(list(hear&see))
 print(len(answer))
 for i in answer : print(i)
