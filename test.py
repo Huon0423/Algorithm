fb3002dd4b71ee4a985ef1945f464c0937e4126b
Yunjeong Cho <dbswjd1413@naver.com>
2023-09-21 23:50:42 +0900

    da

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-# 패턴이 제곱수마다 존재하므로 제곱때마다 추가해준다.
+# input을 리스트에 저장하고, 순서대로 나올 때까지 스택에 저장한다.
+# 만약 스택의 길이가 0이 아니면 input과 스택을 같이 검사한다.
+# 둘 다 순서에 안 맞으면 list의 수를 스택에 저장한다.
 
 import sys
 
 n = int(sys.stdin.readline())
-result = 0
-x = 1
+list = list(map(int, sys.stdin.readline().split()))
+stack = []
+index = 1
 
-while x * x <= n:
-    x += 1
-    result +=1
-
-print(result)
+for i in list :
+    if index == i :
+        index += 1 # 다음 순서로 세팅
+    elif stack[-1] == index :
+        stack.pop()
+    else :
+        stack.append[i]
+if len(stack) > 1 :print("Sad")
+else : print("Nice")
