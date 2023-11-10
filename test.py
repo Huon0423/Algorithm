598b98e87260c75a8bde3660d9d4bb26e6185fca
yunjeong cho <dbswjd1413@naver.com>
2023-11-10 22:50:48 +0900

    오늘은 쉽니다

--- test.py
+++ test.py
@@ -1,14 +1,29 @@
-def recursion(s, l, r):
-    if l >= r: return 1, l+1
-    elif s[l] != s[r]: return 0, l+1
-    else: 
-      return recursion(s, l+1, r-1)
+# 뱀을 다 피하는 방법이 꼭 최단 방법은 아니다.
+# 뱀과 사다리를 간선으로 사용하여 찾는다.
+# BFS를 사용한다.
 
-def isPalindrome(s):
-    return recursion(s, 0, len(s)-1)
+import sys
+from collections import deque
+sys.setrecursionlimit(10**5)
 
-t = int(input())
-for i in range(t):
-  s = input()
-  answer = list(isPalindrome(s))
-  print(answer[0], answer[1])
+n, m = map(int, sys.stdin.readline().split())
+ladders = [] # 사다리 모음
+snakes = [] # 뱀 모음
+dq = deque()
+
+for _ in range(n) :
+    ladders.append(list(map(int, sys.stdin.readline().split())))
+for _ in range(m) :
+    snakes.append(list(map(int, sys.stdin.readline().split())))
+
+def bfs(v,e,r) :
+    b_visited_list.append(str(r)) # 방문 정점 추가
+    v_list[r] = True # 정점 방문 체크
+    dq.append(r) # 큐에 시작 정점 추가
+    while len(dq) > 0 :
+      u = dq.popleft() # 방문한 정점을 큐에서 제거
+      for x in e_list[u] :
+        if not v_list[x]: # 아직 방문하지 않은 인접 정점이라면
+           b_visited_list.append(str(x)) # 방문한 정점의 순서 저장
+           v_list[x] = True # 정점 방문 체크
+           dq.append(x) # 큐에 방문 정점 추가
