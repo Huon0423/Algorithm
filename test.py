15c4de700133d37d41602a48599ec80a4b452ccc
yunjeong cho <dbswjd1413@naver.com>
2023-11-17 00:00:36 +0900

    T = int(input())
    def goTop(s,i,j) :
        global tmp
        while s[i][j] != 0 :
            s[i][j] = tmp
            if i - 1 < 0 : break
            i -= 1
            tmp += 1
        return s
    
    def goBottom(s,i,j) :
        global tmp
        while s[i][j] != 0 :
            s[i][j] = tmp
            if i + 1 >= n : break
            i += 1
            tmp += 1
        return s
    
    def goRight(s,i,j) :
        global tmp
        while s[i][j] != 0 :
            s[i][j] = tmp
            if j + 1 >= n : break
            j += 1
            tmp += 1
            print(j,n)
        return s
    
    def goLeft(s,i,j) :
        global tmp
        while s[i][j] != 0 :
            s[i][j] = tmp
            if j - 1 < 0 : break
            j -= 1
            tmp += 1
        return s
    
    for test_case in range(1, T + 1):
        n = int(input())
        snail = [[0] * n] * n
        i = 0; j = 0
        tmp = 1
        while not (0 in snail) :
            snail = goRight(snail, i, j)
            snail = goBottom(snail, i, j)
            snail = goLeft(snail, i, j)
            sanil = goTop(snail, i, j)
        print(snail)

--- test.py
+++ test.py
@@ -1,30 +1,18 @@
-# 하루에 1개만 살 수 있기에, 제일 매매가가 큰 날 전까지 계속 사는게 좋다.
-# max값을 찾아서 max값의 index를 반환하는 함수를 만든다.
-# list에서 max값을 찾아 그 전까지 매일 구매한다 > 지출을 따로 저장한다.
-# max의 index 이후 값부터 새로 list를 만들어 반복한다.
-# 번 돈은 max값이 바뀌기 전에 max*index - 지출을 sum에 저장한다.
+# 건물을 차례대로 검사하면서, -2,-1,+1,+2만큼의 건물 중 나보다 큰 게 있다면 건너뛴다.
+# 나보다 작다면, 그 중 가장 큰 건물의 높이를 나한테 뺀다 = 세대 수 차이
+# 세대 수를 더해주고 마지막에 반환한다.
 
-T = int(input())
+import sys
 
-def findMaxIndex(p_list) : # max값의 index를 반환해주는 함수
-    index = 0
-    max = p_list[0]
-    for i in range(len(p_list)) :
-        if p_list[i] >= max : max= p_list[i]; index = i
-    return index
+T = 1
 
 for test_case in range(1, T + 1):
-    p_len = int(input())
-    prices = list(map(int, input().split()))
-    p_exp = 0 # 지출
-    p_sum = 0 # 이익
-    while len(prices) > 0 :
-        max_index = findMaxIndex(prices)
-        p_exp = sum(prices[i] for i in range(max_index)) # 최댓값 이전까지의 지출
-        p_sum += prices[max_index] * max_index - p_exp # 지출을 제외한 순이익
-        if len(prices) == max_index + 1 : break # 최댓값이 리스트의 마지막 값이었으면 종료
-        else : prices = prices[max_index+1:] # 아니라면 최댓값 이후부터 리스트 슬라이싱
-    print(p_sum)
-    
+    n = int(input()) # 건물의 개수
+    buildings =list(map(int,input().split())) # 건물의 높이
+    answer = 0
+    for i in range(2, n-2) : # 2부터 시작하고, n-3까지 돌린다 > 건물은 2부터 n-2까지 존재 + 마지막 요소는 n-1이므로 n-1-2
+        max_height = max(buildings[i-2],buildings[i-1],buildings[i+1],buildings[i+2])
+        if max_height < buildings[i] : answer += buildings[i] - max_height
+    print(f"#{test_case}", answer)
         
     
