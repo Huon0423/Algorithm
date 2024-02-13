8c0625d676303e0edcf0766741d9558bab5e0f61
yunjeong cho <97392183+Huon0423@users.noreply.github.com>
2024-02-13 11:50:46 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,8 +1,6 @@
-# 주어진 n에서 5를 최대한 빼고 나머지가 3에 맞아 떨어지는 경우를 출력
-
 n = int(input().strip())
 num = n // 5
 while True :
     if num < 0 : print(-1); break
     if (n - num * 5) % 3 == 0 : print((n - num * 5)//3 + num); break
-    else : num -= 1
+    num -= 1
